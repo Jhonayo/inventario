f54feddb8e5ef316741e9efe50e5a6de0ca7b0cb
jhonayo <j.iramirezpavez@gmail.com>
2025-05-20 02:24:09 -0400

    Primer commit

---
+++ backend/src/main/java/com/papuri/inventario/InventarioOficinaApplication.java
@@ -0,0 +1,13 @@
+package com.papuri.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioOficinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioOficinaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/papuri/inventario/InventarioOficinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papuri.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioOficinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
